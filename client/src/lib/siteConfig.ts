7386f8708d71a50e6a253f77265375c090369831
MeatBoyed <charlierossouw@outlook.com>
2024-08-04 23:03:28 +0200

    feat: MetaData updated

---
+++ client/src/lib/siteConfig.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "UploadShad File/Image Uploader",
+  author: "Charles Rossouw",
+  description: `A highly customizable, accessible, and unopinionated Image Uploader component built with Shadcn UI.`,
+  ogImage: "https://emblor.jaleelbennett.com/og.jpg",
+  url: "https://uploadshad.nerfdesigns.com",
+  links: {
+    twitter: "https://twitter.com/meatboyed",
+    instagram: "https://www.instagram.com/charlesrossouww/",
+    github: "https://github.com/MeatBoyed/Upload-Shad",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
